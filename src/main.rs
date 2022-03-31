70b922dd9194927fd42b0a565fa62be039d23969
jel <25802745+jelni@users.noreply.github.com>
2022-03-31 20:29:05 +0200

    Update eframe to 0.17
    and reorganize imports

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use eframe::{
-    egui::{self, plot},
-    epi,
-};
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console on Windows
+
+use eframe::egui::plot::{Line, Plot, Values};
+use eframe::egui::{CentralPanel, Context, DragValue, Layout, Slider};
+use eframe::epi::{App, Frame};
 
 struct QuadraticPlotter {
     data: Data,
@@ -28,41 +29,41 @@ struct Data {
     c: f64,
 }
 
-impl epi::App for QuadraticPlotter {
+impl App for QuadraticPlotter {
     fn name(&self) -> &str {
         "Quadratic Plotter"
     }
 
-    fn update(&mut self, ctx: &egui::CtxRef, _: &epi::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+    fn update(&mut self, ctx: &Context, _: &Frame) {
+        CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
-                ui.with_layout(egui::Layout::left_to_right(), |ui| {
+                ui.with_layout(Layout::left_to_right(), |ui| {
                     ui.add(
-                        egui::DragValue::new(&mut self.data.a)
+                        DragValue::new(&mut self.data.a)
                             .speed(0.1)
-                            .clamp_range(-100..=100)
+                            .clamp_range::<f64>(-100.0..=100.0)
                             .prefix("a: ")
                             .suffix("x\u{00B2}"),
                     );
                     ui.label("+");
                     ui.add(
-                        egui::DragValue::new(&mut self.data.b)
+                        DragValue::new(&mut self.data.b)
                             .speed(0.1)
-                            .clamp_range(-100..=100)
+                            .clamp_range::<f64>(-100.0..=100.0)
                             .prefix("b: ")
                             .suffix("x"),
                     );
                     ui.label("+");
                     ui.add(
-                        egui::DragValue::new(&mut self.data.c)
+                        DragValue::new(&mut self.data.c)
                             .speed(0.1)
-                            .clamp_range(-100..=100)
+                            .clamp_range::<f64>(-100.0..=100.0)
                             .prefix("c: "),
                     );
                 });
-                ui.with_layout(egui::Layout::right_to_left(), |ui| {
+                ui.with_layout(Layout::right_to_left(), |ui| {
                     ui.add(
-                        egui::Slider::from_get_set(5.0..=501.0, |x| {
+                        Slider::from_get_set(5.0..=501.0, |x| {
                             if let Some(x) = x {
                                 self.quality = (x / 2.).floor() as i16 * 2 + 1;
                             }
@@ -74,12 +75,12 @@ impl epi::App for QuadraticPlotter {
                     );
                 })
             });
-            plot::Plot::new("function_plot")
+            Plot::new("function_plot")
                 .data_aspect(1.)
                 .center_x_axis(true)
                 .show(ui, |plot_ui| {
                     let data = self.data;
-                    plot_ui.line(plot::Line::new(plot::Values::from_explicit_callback(
+                    plot_ui.line(Line::new(Values::from_explicit_callback(
                         move |x| data.a * x.powi(2) + data.b * x + data.c,
                         ..,
                         self.quality as usize,
